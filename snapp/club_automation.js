5f4c1a027d7d55cf923966eb1d53cab465e6fa65
Mathew Karimi <mtk.branding@gmail.com>
2024-06-17 05:55:01 +0330

    Create club_automation.js
    automate getting gifts from snapp club

---
+++ snapp/club_automation.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 100; i++) {
+    setTimeout(() => {
+        document.evaluate("//div[@id='app']/div[2]/div[2]/div[4]/div/div[6]/div/div[2]/div[2]/p", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
+
+        setTimeout(() => {
+            document.evaluate("(//button[@type='button'])[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
+
+            setTimeout(() => {
+                document.evaluate("//img[contains(@src,'https://web-cdn.snapp.ir/club/close.svg')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
+            }, 2000);
+        }, 2000);
+    }, i * 6000); // Each iteration starts every 6 seconds (3 actions * 2 seconds each)
+}
